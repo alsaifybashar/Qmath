8d7c2362beda440eb57e5f831c6e13ef09b5cb69
Alexander Skoglund <64963344+alesk852@users.noreply.github.com>
2024-09-19 17:24:04 +0200

    Revert "Merge branch 'serveradd'"
    This reverts commit b2c3a619b61bf7ff8529c58fa8411cd3860ce806, reversing
    changes made to d4a032e8f8545b321b88f3bcf4bf71acaca97696.

---
+++ web-app/Controllers/WeatherForecastController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_app.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class QuestionsController : ControllerBase
+{
+    private static readonly string[] Questions = new []
+    {
+        "x+1=2",
+        "x^2+4x+4=0",
+        "x+3=5"
+    };
+
+    private readonly ILogger<QuestionsController> _logger;
+
+    public QuestionsController(ILogger<QuestionsController> logger) 
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Questions> Get()
+    {
+        return Enumerable.Range(0, Questions.Length).Select(index => new Questions
+        {
+            QuestionText = Questions[index]
+        }).ToArray();
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class AnswersController : ControllerBase
+{
+
+    private static readonly string[] Answers = new []
+    {
+        "1",
+        "-2",
+        "2"
+    };
+
+    private readonly ILogger<AnswersController> _logger;
+
+    public AnswersController(ILogger<AnswersController> logger) 
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Answers> Get()
+    {
+        return Enumerable.Range(0,Answers.Length).Select(index => new Answers{
+            AnswersText = Answers[index]
+        }).ToArray();
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class SubQuestionsController : ControllerBase
+{
+
+    private static readonly string[][] SubQuestions = new []
+    {
+        new string[] {"sub question 1", "subquestion 2"},
+        new string[] {"sub question 1", "subquestion 2"},
+        new string[] {"sub question 1", "subquestion 2"}
+    };
+
+    private readonly ILogger<SubQuestionsController> _logger;
+
+    public SubQuestionsController(ILogger<SubQuestionsController> logger) 
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<SubQuestions> Get()
+    {
+        return Enumerable.Range(0,SubQuestions.Length).Select(index => new SubQuestions{
+            subQuestionText = SubQuestions[index]
+        }).ToArray();
+    }
+}
+
--- web-app/Program.cs
+++ web-app/Program.cs
@@ -1,44 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using web_app.Model.context;
-using web_app.Model.Context;
-using web_app.Repository;
-using web_app.Repository.IRepository;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
-builder.Services.AddScoped<ISubQuestionRepository, SubQuestionRepository>();
-builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
-
-
-
-builder.Services.AddDbContext<QuestionContext>(options =>
-{
-    options.UseSqlServer("server = DESKTOP-IHI3C6H\\SQLEXPRESS;database=QMath;trusted_connection=true;trustservercertificate=true;",
-     o => o.MigrationsHistoryTable(
-            tableName: HistoryRepository.DefaultTableName,
-            schema: "Question"));
-});
-
-builder.Services.AddDbContext<SubQuestionContext>(options =>
-{
-    options.UseSqlServer("server = DESKTOP-IHI3C6H\\SQLEXPRESS;database=QMath;trusted_connection=true;trustservercertificate=true;",
-            o => o.MigrationsHistoryTable(
-            tableName: HistoryRepository.DefaultTableName,
-            schema: "SubQuestion"));
-});
-
-builder.Services.AddDbContext<AnswersContext>(options =>
-{
-    options.UseSqlServer("server = DESKTOP-IHI3C6H\\SQLEXPRESS;database=QMath;trusted_connection=true;trustservercertificate=true;",
-             o => o.MigrationsHistoryTable(
-            tableName: HistoryRepository.DefaultTableName,
-            schema: "Answer"));
-});
 
 var app = builder.Build();
 
@@ -55,7 +19,7 @@ app.UseRouting();
 
 
 app.MapControllerRoute(
-    name: "deafult",
+    name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");
